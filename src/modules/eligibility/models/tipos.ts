4524346db847b2ee2dd9108e6694996783979a57
eduardojabes <eduardojabes@hotmail.com>
2022-11-29 13:48:06 -0300

    feat: added Lemon initial files

---
+++ src/modules/eligibility/models/tipos.ts
@@ -0,0 +1,33 @@
+export {};
+
+const cpf = {
+    type: 'string',
+    pattern: '^\\d{11}$',
+    example: '21554495008',
+}
+  
+const cnpj = {
+    type: 'string',
+    pattern: '^\\d{14}$',
+    example: '33400689000109',
+}
+  
+const tiposDeConexao = ['monofasico', 'bifasico', 'trifasico']
+  
+const classesDeConsumo = [
+    'residencial',
+    'industrial',
+    'comercial',
+    'rural',
+    'poderPublico',
+  ]
+  
+const modalidadesTarifarias = ['azul', 'branca', 'verde', 'convencional']
+  
+module.exports = {
+    tiposDeConexao,
+    classesDeConsumo,
+    modalidadesTarifarias,
+    cpf,
+    cnpj,
+ }
